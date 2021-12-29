3770df03fa4d8c365cacfc3093aa07fae821b99e
Martin Hartl <martin@hartl.co>
2021-12-29 09:32:02 +0100

    Reenable image insertion

--- Loc.swiftpm/Store/TimelineStore.swift
+++ Loc.swiftpm/Store/TimelineStore.swift
@@ -47,18 +47,18 @@ final class TimelineStore: ObservableObject {
 
                 // TODO: OMG fix
                 let photos = self.photoService.photos(from: Date(),
-                                                      back: 2)
+                                                      back: 5)
 
                 var dict = [Date: [Entry]]()
 
-//                for index in 0..<photos.count {
-//                    let photo = photos[index]
-//                    var entries = dict[photo.creationDate!.simplifiedDate()] ?? []
-//
-//                    entries.append(.photoSuggestion([photo]))
-//
-//                    dict[photo.creationDate!.simplifiedDate()] = entries
-//                }
+                for index in 0..<photos.count {
+                    let photo = photos[index]
+                    var entries = dict[photo.creationDate!.simplifiedDate()] ?? []
+
+                    entries.append(.photoSuggestion([photo]))
+
+                    dict[photo.creationDate!.simplifiedDate()] = entries
+                }
 
                 let mappedEntries: [TimelineStore.Day] = days.map { day in
                     var entries = [Entry]()
